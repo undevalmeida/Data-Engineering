7bbe6970d406ca98b052803782d74ebe1601da56
André Almeida <andrejunior573@gmail.com>
2026-03-18 23:01:26 -0300

    Add hooks module for custom integrations
    - Introduced a new __init__.py file in the plugins/hooks directory to define and export custom hooks for the project.
    - Included FootballDataOrgHook and PostgresHook to facilitate data integration with external services and PostgreSQL databases.

---
+++ plugins/hooks/__init__.py
@@ -0,0 +1,6 @@
+# Hooks customizados
+
+from plugins.hooks.football_data_org_hook import FootballDataOrgHook
+from plugins.hooks.postgres_hook import PostgresHook
+
+__all__ = ["FootballDataOrgHook", "PostgresHook"]
